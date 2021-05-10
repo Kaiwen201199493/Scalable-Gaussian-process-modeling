be18943530a7a89ef7e562a9fef377de63dca76d
Kaiwen201199493 <83950890+Kaiwen201199493@users.noreply.github.com>
2021-05-10 18:22:10 +0800

    LLE Select sample points then dimension reduction

--- LLEtry.m
+++ LLEtry.m
@@ -1,5 +1,5 @@
 clearvars;clc;close all;
-addpath('dace');%引用文件夹
+addpath('dace');
 fun_name = 'Ellipsoid';
 d = 100;
 lower_bound = -5.12*ones(1,d);%下限
@@ -12,12 +12,12 @@ upper_bound = 5.12*ones(1,d);%上限
 for i = 1:10
 tic%保存当前时间
 % sampling
- num_sample = 1000; %采样的数量
-    sample_x = lhsdesign(num_sample,d).*(upper_bound - lower_bound)+lower_bound;%lhsdesign拉丁超立方采样,代码中是一个2(行) x 100(列)的矩阵;*符号是对矩阵;.*符号是对数组
+ num_sample = 1000; 
+    sample_x = lhsdesign(num_sample,d).*(upper_bound - lower_bound)+lower_bound;
     sample_y = feval(fun_name,sample_x);
     num_train = 100;
 if size(sample_x,1) > num_train
-   sample_x_select = zeros(num_train,d);%初始化 数量 X 维数的矩阵;
+   sample_x_select = zeros(num_train,d);%
    sample_x_temp = sample_x;
    a = size(sample_x,1);
    indx = randi(size(sample_x,1));
@@ -36,7 +36,7 @@ end
     sample_y_select = feval(fun_name,sample_x_select);
     GP_model = dacefit(test',sample_y_select, 'regpoly0', 'corrgauss', 1*ones(1,dim), 0.001*ones(1,dim), 1000*ones(1,dim));
     
- test_sample = 1000;%测试点的数量
+ test_sample = 1000;
      test_x = lhsdesign(test_sample,d).*(upper_bound - lower_bound)+lower_bound;
      test_points = lle(test_x',k,dim);
      test_y = feval(fun_name,test_x);
