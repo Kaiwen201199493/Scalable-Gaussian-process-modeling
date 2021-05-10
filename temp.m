2583b2eb8e6d86665c0f94ede4e4fbbe4fdafdfd
Kaiwen201199493 <83950890+Kaiwen201199493@users.noreply.github.com>
2021-05-10 17:53:14 +0800

    Select a subset of sample points

--- temp.m
+++ temp.m
@@ -1,22 +1,22 @@
 
 clearvars;clc;close all;
-addpath('dace');%引用文件夹
+addpath('dace');
 fun_name = 'Ellipsoid';
 d = 10;
-lower_bound = -5.12*ones(1,d);%下限
-upper_bound = 5.12*ones(1,d);%上限
+lower_bound = -5.12*ones(1,d);
+upper_bound = 5.12*ones(1,d);
 
  time =zeros(1,10);
  RMSE = zeros(1,10);
-for i = 1:1
+for i = 1:10
 tic%保存当前时间
 % sampling
- num_sample = 1000; %采样的数量
-    sample_x = lhsdesign(num_sample,d).*(upper_bound - lower_bound)+lower_bound;%lhsdesign拉丁超立方采样,代码中是一个2(行) x 100(列)的矩阵;*符号是对矩阵;.*符号是对数组
+ num_sample = 1000; 
+    sample_x = lhsdesign(num_sample,d).*(upper_bound - lower_bound)+lower_bound;
     sample_y = feval(fun_name,sample_x);
     num_train = 100;
 if size(sample_x,1) > num_train
-    sample_x_select = zeros(num_train,d);%初始化 数量 X 维数的矩阵;
+    sample_x_select = zeros(num_train,d);
     sample_x_temp = sample_x;
     a = size(sample_x,1);
     indx = randi(size(sample_x,1));
@@ -34,14 +34,14 @@ end
     sample_y_select = feval(fun_name,sample_x_select);
     GP_model = dacefit(sample_x_select,sample_y_select, 'regpoly0', 'corrgauss', 1*ones(1,d), 0.001*ones(1,d), 1000*ones(1,d));
     
- test_sample = 1000;%测试点的数量
+ test_sample = 1000;
      test_x = lhsdesign(test_sample,d).*(upper_bound - lower_bound)+lower_bound;
      test_y = feval(fun_name,test_x);
      
  test_predition = predictor(test_x,GP_model);
 
  RMSe=sqrt(sum((test_predition- test_y).^2)/test_sample);
- toc;%记录程序完成时间  
+ toc;  
  RMSE(i)=RMSe;
  time(i)=toc;
 end
